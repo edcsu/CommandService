27dbe5d7a28a30d1b19a667de138a90f00c77c34
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-04-15 18:16:14 +0300

    feat: create DTOs

---
+++ CommandService/Business/ViewModels/CommandDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CommandService.Business.ViewModels
+{
+    public class CommandDetailsDto
+    {
+        public Guid Id { get; set; }
+
+        public string? HowTo { get; set; }
+
+        public string? CommandLine { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Updated { get; set; }
+    }
+}
---
+++ CommandService/Business/ViewModels/PlatformDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandService.Business.ViewModels
+{
+    public class PlatformDetailsDto
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Updated { get; set; }
+    }
+}
